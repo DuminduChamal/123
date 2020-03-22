cc67d9315b9b383486dc55846bc8b5cc7dc6e297
dumindu96 <36619804+dumindu96@users.noreply.github.com>
2020-03-22 12:06:52 +0530

    final version2

--- src/pkgnew/DotCom/DotComBust.java
+++ src/pkgnew/DotCom/DotComBust.java
@@ -10,6 +10,56 @@ public class DotComBust
 
     private void setUpGame()
     {
+        DotCom one = new DotCom();
+        one.setName("Pets.com");
         
+        DotCom two = new DotCom();
+        two.setName("eToys.com");
+
+        DotCom three = new DotCom();
+        three.setName("Go2.com");
+
+        dotComsList.add(one);
+        dotComsList.add(two);
+        dotComsList.add(three);
+
+        System.out.println("Your goal is to sink three dotcoms.");
+        System.out.println("Pets.com, eToys.com and Go2.com");
+        System.out.println("Try to sink them all in fewest number of guesses!");
+
+        for(DotCom dotComToSet : dotComsList)
+        {
+            ArrayList<String> newLocation = helper.placeDotCom(3);
+            dotComToSet.setLocationCells(newLocation);
+        }
     }
+
+    private void startPlaying()
+    {
+        while(!dotComsList.isEmpty())
+        {
+            String userGuess = helper.getUserInput("Enter a guess : ");
+            checkUserGuess(userGuess);
+        }
+        finishGame();
+    }
+
+    private void checkUserGuess(String userGuess)
+    {
+        numOfGuesses++;
+        String result = "miss";
+        for(DotCom dotComToTest : dotComsList)
+        {
+            result = dotComToTest.checkYourself(userGuess);
+            if(result.equals("hit"))
+                break;
+            if(result.equals("kill"))
+            {
+                dotComsList.remove(dotComToTest);
+                break;
+            }
+        }
+        System.out.println(result);
+    }
+
 }
