9140cc6cef6c8fab0353afffcd7930df60021cc0
dumindu96 <36619804+dumindu96@users.noreply.github.com>
2020-03-23 22:32:21 +0530

    constructor5

--- Const/SonOfBoo.java
+++ Const/SonOfBoo.java
@@ -20,10 +20,10 @@ class SonOfBoo extends Boo
 
     // }
 
-    // public SonOfBoo(String a, String b, String c)
-    // {
-    //     super(a, b);
-    // }
+    public SonOfBoo(String a, String b, String c)
+    {
+        super(a, b);
+    }
 
     // public SonOfBoo(int i, int j)
     // {
