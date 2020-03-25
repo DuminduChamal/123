231d3e250bbae050836e2bf315ccf07492172194
dumindu96 <36619804+dumindu96@users.noreply.github.com>
2020-03-26 00:37:32 +0530

    gui3

--- GUI/MyDrawPanel.java
+++ GUI/MyDrawPanel.java
@@ -6,20 +6,23 @@ class MyDrawPanel extends JPanel
 {
     public void paintComponent(Graphics g)
     {
-        Graphics2D g2d = (Graphics2D) g;
+        // Graphics2D g2d = (Graphics2D) g;
 
-        int red = (int)(Math.random()*255);
-        int green = (int)(Math.random()*255);
-        int blue = (int)(Math.random()*255);
-        Color startColor = new Color(red, green, blue);
+        // int red = (int)(Math.random()*255);
+        // int green = (int)(Math.random()*255);
+        // int blue = (int)(Math.random()*255);
+        // Color startColor = new Color(red, green, blue);
 
-        red = (int)(Math.random()*255);
-        green = (int)(Math.random()*255);
-        blue = (int)(Math.random()*255);
-        Color endColor = new Color(red, green, blue);
+        // red = (int)(Math.random()*255);
+        // green = (int)(Math.random()*255);
+        // blue = (int)(Math.random()*255);
+        // Color endColor = new Color(red, green, blue);
 
-        GradientPaint gradient = new GradientPaint(70,70,startColor,150,150,endColor);
-        g2d.setPaint(gradient);
-        g2d.fillOval(70, 70, 100, 100);
+        // GradientPaint gradient = new GradientPaint(70,70,startColor,150,150,endColor);
+        // g2d.setPaint(gradient);
+        // g2d.fillOval(70, 70, 100, 100);
+
+            g.setColor(Color.orange);
+            g.fillRect(20, 50, 100, 100);
     }
 }
