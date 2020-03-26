f61eb41e7f3a08b008ed2d3a89fa945bc34c6b8e
dumindu96 <36619804+dumindu96@users.noreply.github.com>
2020-03-27 00:53:09 +0530

    layout2

---
+++ GUI/Check2.java
@@ -0,0 +1,28 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class Check2
+{
+    public static void main(String[] args)
+    {
+        JFrame frame = new JFrame();
+        JPanel panel = new JPanel();
+        panel.setBackground(Color.darkGray);
+
+        panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
+
+        JButton btn1 = new JButton("Shock me");
+        JButton btn2 = new JButton("Bliss");
+        JButton btn3 = new JButton("HUh?");
+        JButton btn4 = new JButton("What is this?");
+
+        panel.add(btn1);
+        panel.add(btn2);
+        panel.add(btn3);
+        panel.add(btn4);
+
+        frame.getContentPane().add(BorderLayout.EAST, panel);
+        frame.setSize(250,200);
+        frame.setVisible(true);
+    }
+}
