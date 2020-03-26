a7f085e17b7e144abfd67d1d422bd4c405400024
dumindu96 <36619804+dumindu96@users.noreply.github.com>
2020-03-26 21:23:12 +0530

    innerClass

---
+++ GUI/InnerButton.java
@@ -0,0 +1,39 @@
+import javax.swing.*;
+import java.awt.event.*;
+import java.awt.*;
+
+class InnerButton
+{
+    JFrame frame;
+    JButton b;
+
+    public static void main(String[] args)
+    {
+        InnerButton gui = new InnerButton();
+        gui.go();
+    }
+
+    public void go()
+    {
+        frame = new JFrame();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        b = new JButton("A");
+        b.addActionListener(new BLis());
+
+        frame.getContentPane().add(BorderLayout.SOUTH, b);
+        frame.setSize(200, 100);
+        frame.setVisible(true);
+    }
+
+    class BLis implements ActionListener
+    {
+        public void actionPerformed(ActionEvent e)
+        {
+            if(b.getText().equals("A"))
+                b.setText("B");
+            else
+                b.setText("A");
+        }
+    }
+}
