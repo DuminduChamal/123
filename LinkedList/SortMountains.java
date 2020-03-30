4a9049adf2c1bb8c26cf59e96e7ab7ddcbff8db7
dumindu96 <36619804+dumindu96@users.noreply.github.com>
2020-03-30 23:17:04 +0530

    linked list with java2

--- LinkedList/SortMountains.java
+++ LinkedList/SortMountains.java
@@ -4,7 +4,7 @@ public class SortMountains
 {
     LinkedList<Mountain> mtn = new LinkedList<Mountain>();
 
-    class nameCompare implements Comparator<Mountain>
+    class NameCompare implements Comparator<Mountain>
     {
         public int compare(Mountain one, Mountain two)
         {
@@ -26,6 +26,19 @@ public class SortMountains
 
     public void go()
     {
-        
+        mtn.add(new Mountain("Long",14255));
+        mtn.add(new Mountain("Elbert",14433));
+        mtn.add(new Mountain("Maroon",14155));
+        mtn.add(new Mountain("Castle",14265));
+
+        System.out.println("As entered:\n"+mtn);
+
+        NameCompare nc = new NameCompare();
+        Collection.sort(mtn,nc);
+        System.out.println("By name:\n"+mtn);
+
+        HeightCompare hc = new HeightCompare();
+        Collection.sort(mtn,hc);
+        System.out.println("By height:\n"+mtn);
     }
 }
