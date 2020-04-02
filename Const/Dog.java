ee311f34d94f15e5e8925d7444296fce168e31be
dumindu96 <36619804+dumindu96@users.noreply.github.com>
2020-04-02 23:32:15 +0530

    size affects bark - dog class

---
+++ Const/Dog.java
@@ -0,0 +1,21 @@
+class Dog
+{
+    int size;
+    String name;
+
+    void bark()
+    {
+        if(size>60)
+        {
+            System.out.println("Woof Woof!");
+        }
+        else if(size>14)
+        {
+            System.out.println("Ruff! Ruff!");
+        }
+        else
+        {
+            System.out.println("Yip! Yup!");
+        }
+    }
+}
