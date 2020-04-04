07cce7ec1e7e61b201e0725d432d27e2f999dd3d
dumindu96 <36619804+dumindu96@users.noreply.github.com>
2020-04-04 23:07:26 +0600

    final

--- Poly/Dog.java
+++ Poly/Dog.java
@@ -4,6 +4,7 @@ public class Dog extends Animal {
     public void makeNoice()
     {
         System.out.println("Dog is barking");
+        System.out.println("Bawww Bawwww");
     }
 
 }
--- Poly/Hippo.java
+++ Poly/Hippo.java
@@ -2,7 +2,7 @@
 public class Hippo extends Animal{
     public void makeNoice()
     {
-        System.out.println("Hippo is making sound");
+        System.out.println("Hippo is making sound!!");
     }
 
 }
