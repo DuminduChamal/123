15e6d42c87dd62c2ba066f0ef5a690b69323ce88
dumindu96 <36619804+dumindu96@users.noreply.github.com>
2020-04-07 21:38:17 +0530

    driver cls

---
+++ RunTimePolymorphism/Test.java
@@ -0,0 +1,12 @@
+
+// Driver class 
+public class Test 
+{ 
+    public static void main(String args[]) 
+    { 
+        D d = new E(); // object of type B 
+  
+        // Data member of class A will be accessed 
+        System.out.println(d.x); 
+    } 
+} 
