6470d88b689fb0fadd325fc5ef38aece293eee3d
dumindu96 <36619804+dumindu96@users.noreply.github.com>
2020-04-08 23:03:05 +0530

    date demo

---
+++ Time/DateDemo4.java
@@ -0,0 +1,12 @@
+
+import java.util.Date;
+public class DateDemo4 {
+
+   public static void main(String args[]) {
+      // Instantiate a Date object
+      Date date = new Date();
+  
+      // display time and date
+      System.out.printf("%1$s %2$tB %2$td, %2$tY", "Due date:", date);
+   }
+}
