8194355c7264d750dc020c52fffd1ac527803baf
dumindu96 <36619804+dumindu96@users.noreply.github.com>
2020-04-09 23:55:25 +0530

    thread demo

---
+++ thread/TestThread2.java
@@ -0,0 +1,10 @@
+public class TestThread2 {
+
+   public static void main(String args[]) {
+      ThreadDemo T1 = new ThreadDemo( "Thread-1");
+      T1.start();
+      
+      ThreadDemo T2 = new ThreadDemo( "Thread-2");
+      T2.start();
+   }   
+}
