641ea5d98de977a2553bab34bb5c6bc8651129b1
dumindu96 <36619804+dumindu96@users.noreply.github.com>
2020-04-10 01:11:07 +0530

    abstract

---
+++ Abstract/AbstractDemo.java
@@ -0,0 +1,9 @@
+public class AbstractDemo {
+
+   public static void main(String [] args) {
+      /* Following is not allowed and would raise error */
+      Employee e = new Employee("George W.", "Houston, TX", 43);
+      System.out.println("\n Call mailCheck using Employee reference--");
+      e.mailCheck();
+   }
+}
