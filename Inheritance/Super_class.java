d21d12aed0883c9f9e175ff2d463e5cc6a6426e5
dumindu96 <36619804+dumindu96@users.noreply.github.com>
2020-04-11 01:25:46 +0530

    inheritance

---
+++ Inheritance/Super_class.java
@@ -0,0 +1,8 @@
+class Super_class {
+   int num = 20;
+
+   // display method of superclass
+   public void display() {
+      System.out.println("This is the display method of superclass");
+   }
+}
