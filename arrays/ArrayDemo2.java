795a4fa7df3b5586dec38172798ecdb701c272da
dumindu96 <36619804+dumindu96@users.noreply.github.com>
2020-04-14 21:34:35 +0530

    array2

---
+++ arrays/ArrayDemo2.java
@@ -0,0 +1,12 @@
+class ArrayDemo2 {
+   public static void passByReference(String a[]){
+     a[0] = "Changed";
+   }
+ 
+   public static void main(String args[]){
+      String []b={"Apple","Mango","Orange"};
+      System.out.println("Before Function Call    "+b[0]);
+      ArrayDemo.passByReference(b);
+      System.out.println("After Function Call    "+b[0]);
+   }
+}
