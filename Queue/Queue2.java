8dd7ca8486ab565a45679b99495b360d38f06a5f
dumindu96 <36619804+dumindu96@users.noreply.github.com>
2020-04-17 12:33:10 +0530

    queue

--- Queue/Queue2.java
+++ Queue/Queue2.java
@@ -1,15 +1,18 @@
+// Java code to illustrate 
+// methods of AbstractQueue 
+  
 import java.util.*; 
 import java.util.concurrent.LinkedBlockingQueue; 
   
-public class Queue1 { 
+public class Queue2 { 
     public static void main(String[] argv) 
         throws Exception 
     { 
         // Creating object of AbstractQueue<Integer> 
         AbstractQueue<Integer> 
             AQ = new LinkedBlockingQueue<Integer>(); 
-
-        // Populating AQ 
+  
+        // Populating AQ using add() method 
         AQ.add(10); 
         AQ.add(20); 
         AQ.add(30); 
@@ -19,5 +22,12 @@ public class Queue1 {
         // print AQ 
         System.out.println("AbstractQueue contains: "
                            + AQ); 
+  
+        // Get the head of Queue using element() method 
+        System.out.println("Head: " + AQ.element()); 
+  
+        // Clear the Queue using clear() method 
+        AQ.clear(); 
+        System.out.println("AbstractQueue: " + AQ); 
     } 
 }
