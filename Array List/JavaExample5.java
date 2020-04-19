ade5119bb5e2b39a6a09127d08ce2acd2043c858
dumindu96 <36619804+dumindu96@users.noreply.github.com>
2020-04-20 01:26:15 +0530

    arraylist

---
+++ Array List/JavaExample5.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+
+public class JavaExample5 {
+   public static void main(String[] args) {
+      ArrayList<Integer> numbers = new ArrayList<Integer>();
+      numbers.add(1);
+      numbers.add(7);
+      numbers.add(5);
+      numbers.add(6);
+      System.out.println("Number of elements in ArrayList: "+numbers.size());
+   }
+}
