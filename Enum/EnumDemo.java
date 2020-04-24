36453f2b6ee4fd5d016554dfb87c4393b9118f9c
dumindu96 <36619804+dumindu96@users.noreply.github.com>
2020-04-25 01:49:39 +0530

    arraylist

---
+++ Enum/EnumDemo.java
@@ -0,0 +1,21 @@
+ublic enum Directions{
+	  EAST, 
+	  WEST, 
+	  NORTH, 
+	  SOUTH
+}
+public class EnumDemo
+{
+   public static void main(String args[]){
+	Directions dir = Directions.NORTH;  
+	if(dir == Directions.EAST) {
+	    System.out.println("Direction: East");
+	} else if(dir == Directions.WEST) {
+	    System.out.println("Direction: West");
+	  } else if(dir == Directions.NORTH) {
+	      System.out.println("Direction: North");
+  	    } else {
+		System.out.println("Direction: South");
+	      }
+   }
+}
