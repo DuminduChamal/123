5875ec2af82e7be60f90550f1b0dde9836a4e17e
dumindu96 <36619804+dumindu96@users.noreply.github.com>
2020-04-25 01:47:29 +0530

    arraylist

---
+++ Queue/MyComparator.java
@@ -0,0 +1,10 @@
+import java.util.Comparator;
+
+public class MyComparator implements Comparator<String>
+{
+   @Override
+   public int compare(String x, String y)
+   {
+      return x.length() - y.length();
+   }
+}
