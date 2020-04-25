0da0abf1d21e31ddaf6d7cca608ceee38eb19be0
dumindu96 <36619804+dumindu96@users.noreply.github.com>
2020-04-26 01:56:51 +0530

    arraylist

---
+++ Regex/RegexExampleMatcher.java
@@ -0,0 +1,16 @@
+import java.util.regex.*;  
+class RegexExampleMatcher{  
+    public static void main(String args[]){  
+        String content = "ZZZ AA PP AA QQQ AAA ZZ";
+
+        String string = "AA";
+        Pattern pattern = Pattern.compile(string);
+        Matcher matcher = pattern.matcher(content);
+
+        while(matcher.find()) {
+            System.out.println("Found at: "+ matcher.start()
+                    + 
+                    " - " + matcher.end());
+        }
+    }
+}
