3d9ccc9f65575cbd99f5005bfd68565b984459cc
dumindu96 <36619804+dumindu96@users.noreply.github.com>
2020-04-27 02:06:09 +0530

    lambda

---
+++ Lambda/Example3.java
@@ -0,0 +1,15 @@
+import java.util.*;  
+public class Example3{  
+    public static void main(String[] args) {       
+       List<String> list=new ArrayList<String>();  
+       list.add("Rick");         
+       list.add("Negan");       
+       list.add("Daryl");         
+       list.add("Glenn");         
+       list.add("Carl");                
+       list.forEach(          
+           // lambda expression        
+           (names)->System.out.println(names)         
+       );     
+    }  
+}
