eae4fcc6e41f5f141b90d53f0fefdf77e30eb0c8
dumindu96 <36619804+dumindu96@users.noreply.github.com>
2020-05-04 16:42:41 +0530

    io

---
+++ Files and IO/BufferedOutputStreamExample.java
@@ -0,0 +1,14 @@
+import java.io.*;  
+public class BufferedOutputStreamExample{    
+public static void main(String args[])throws Exception{    
+     FileOutputStream fout=new FileOutputStream("D:\\testout.txt");    
+     BufferedOutputStream bout=new BufferedOutputStream(fout);    
+     String s="Welcome to javaTpoint.";    
+     byte b[]=s.getBytes();    
+     bout.write(b);    
+     bout.flush();    
+     bout.close();    
+     fout.close();    
+     System.out.println("success");    
+}    
+}  
