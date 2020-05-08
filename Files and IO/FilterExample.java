ed61c9f33fa5e1068881213168e84ff7d1613fe9
dumindu96 <36619804+dumindu96@users.noreply.github.com>
2020-05-08 16:49:37 +0530

    io

---
+++ Files and IO/FilterExample.java
@@ -0,0 +1,17 @@
+import java.io.*;  
+
+
+public class FilterExample {  
+    public static void main(String[] args) throws IOException {  
+        File data = new File("D:\\testout.txt");  
+        FileOutputStream file = new FileOutputStream(data);  
+        FilterOutputStream filter = new FilterOutputStream(file);  
+        String s="Welcome to javaTpoint.";      
+        byte b[]=s.getBytes();      
+        filter.write(b);     
+        filter.flush();  
+        filter.close();  
+        file.close();  
+        System.out.println("Success...");  
+    }  
+}
